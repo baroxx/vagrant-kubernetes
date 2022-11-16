19ba7bc408b1894fa5a415e086bea919de158dae
Sebastian Erfurth <se.erfurth@gmail.com+>
2022-11-16 21:04:54 +0100

    Fixed config errors of runtime/image endpoints

--- provisioner/containerd.sh
+++ provisioner/containerd.sh
@@ -7,4 +7,6 @@ mkdir /etc/containerd
 containerd config default > /etc/containerd/config.toml
 systemctl restart containerd
 
+crictl config --set runtime-endpoint=unix:///run/containerd/containerd.sock --set image-endpoint=unix:///run/containerd/containerd.sock
+
 echo -e "finished..."
