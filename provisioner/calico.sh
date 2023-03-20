8ef27a2c65d754650eaa676a3b5a8c647454d354
Sebastian Erfurth <se.erfurth@gmail.com+>
2023-03-20 13:55:10 +0100

    Fixed source for the calico manifests

--- provisioner/calico.sh
+++ provisioner/calico.sh
@@ -3,7 +3,7 @@ set -e
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-wget https://docs.projectcalico.org/manifests/calico.yaml -O calico.yaml
+curl https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/calico.yaml -O
 kubectl apply -f calico.yaml
 rm calico.yaml
 
